b6b320ad03d00b6f9a1e3a1ece84e9daff2695f3
liupeng <LIUPENG.0@outlook.com>
2018-10-17 23:29:14 +0800

    wi

--- Weibo/Classes/View/Main/lpWBMainViewController.swift
+++ Weibo/Classes/View/Main/lpWBMainViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+// 主控制器
 class lpWBMainViewController: UITabBarController {
 
     override func viewDidLoad() {
@@ -28,3 +29,46 @@ class lpWBMainViewController: UITabBarController {
     */
 
 }
+// extension 类似于 OC 的分类， 在Swift中可以切分代码块
+// 可以把相近功能的函数， 放在一个 extenstion 中
+// 便于代码的维护
+// MARK： - 设置界面
+extension lpWBMainViewController {
+    
+    let array = [
+        ["clsName": "", "title": "首页", "imageName": ""],
+        
+    ]
+    
+    var
+    
+    ///设置所有的子控件
+    private func setupChildController() {
+        
+    }
+    
+    /// 使用s字典创建一个字控制器
+    ///
+    /// - parameter dict : 字典信息[clsName, title, imageName]
+    ///
+    /// = returns: 子控制器
+    private func controller(dict: [String : String]) -> UIViewController {
+        
+        // 1. 获取字典内容
+        guard let clsName = dict["clsName"],
+            let title = dict["title"],
+            let imageName = dict["imageName"]
+            
+            else {
+            return UIViewController()
+        }
+        
+        // 2. 创建视图控制器
+        // 1> 将 clsName 转化成 cls
+        let cls = NSClassFromString(Bundle.main().namespace + "." + clsName) as? UIViewController.Type
+        
+        
+        return UIViewController()
+    }
+}
+
---
+++ Weibo/lpWeibo_Bribge.h
@@ -0,0 +1,15 @@
+//
+//  lpWeibo_Bribge.h
+//  Weibo
+//
+//  Created by 刘鹏 on 2018/10/17.
+//  Copyright © 2018 刘鹏. All rights reserved.
+//
+
+#ifndef lpWeibo_Bribge_h
+#define lpWeibo_Bribge_h
+
+
+#endif /* lpWeibo_Bribge_h */
+// 桥接文件， swifty使用OC 宏不可以使用
+//#import 
