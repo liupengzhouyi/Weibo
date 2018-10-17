46fabb39eab8842f9b013a74b5eec4fe89a603e3
liupeng <LIUPENG.0@outlook.com>
2018-10-17 20:30:25 +0800

    建立目录&创建页面

--- Weibo/AppDelegate.swift
+++ Weibo/AppDelegate.swift
@@ -16,6 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        sleep(3)
+        
+        window = UIWindow()
+        window?.backgroundColor = UIColor.red
+        
+        window?.rootViewController = lpWBMainViewController()
+        window?.makeKeyAndVisible()
+        
+        
         return true
     }
 
---
+++ Weibo/Classes/View/Main/lpWBBaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  lpWBBaseViewController.swift
+//  Weibo
+//
+//  Created by 刘鹏 on 2018/10/17.
+//  Copyright © 2018 刘鹏. All rights reserved.
+//
+
+import UIKit
+
+class lpWBBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
